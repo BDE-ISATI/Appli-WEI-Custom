c687ae44d4f8ef50633072034186af87503fdb6e
Feldrise <admin@feldrise.com>
2020-09-08 00:12:53 +0200

    [Fix] Refresh player score on home page

--- lib/src/pages/home_page/widgets/user_score.dart
+++ lib/src/pages/home_page/widgets/user_score.dart
@@ -1,3 +1,5 @@
+import 'package:appli_wei_custom/models/user.dart';
+import 'package:appli_wei_custom/services/user_service.dart';
 import 'package:appli_wei_custom/src/providers/user_store.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -9,7 +11,23 @@ class UserScore extends StatelessWidget {
       builder: (context, userStore, child) {
         return Column(
           children: [
-            Text(userStore.score.toString(), style: TextStyle(fontSize: 128, color: Theme.of(context).accentColor),),
+            FutureBuilder(
+              future: UserService.instance.getUser(userStore.authentificationHeader, userStore.id),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (!snapshot.hasData) {
+                    return const Text("Une erreur s'est produite...");
+                  }
+
+                  final User player = snapshot.data as User;
+                  return Text(player.score.toString(), style: TextStyle(fontSize: 128, color: Theme.of(context).accentColor),);
+
+                }
+                else {
+                  return const CircularProgressIndicator();
+                }
+              },
+            ),
             const SizedBox(height: 4,),
             const Text("Ton score")
           ],
